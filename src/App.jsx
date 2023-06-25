2f4714e9bfeb371d8efc427d16eb6f54e7bc3fee
Alirahmon777 <alirahmonuraliev007@gmail.com>
2023-06-25 20:52:13 +0500

    ozgardi

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
-
-export default function App() {
-  return <div>App</div>;
-<<<<<<< HEAD
-=======
-=======
-import React from 'react'
-import Login from "./pages/Login/Login"
+import Login from './pages/Login/Login';
 export default function App() {
   return (
-    <div>
-<Login/>
-    </div>
-  )
->>>>>>> 9eb4935ec50a9c57d9a4e84e237f2ded02689f2e
->>>>>>> ae7c4460cfce5f89746b430c4eacd96be93c4676
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  );
 }
