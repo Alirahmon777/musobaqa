f389fc21d4565ab07a5c226d26302c28cc294211
Alirahmon777 <alirahmonuraliev007@gmail.com>
2023-06-25 20:27:48 +0500

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
 import React from 'react';
 
 export default function App() {
   return <div>App</div>;
-=======
-import React from 'react'
-
-export default function App() {
-  return (
-    <div>App</div>
-  )
->>>>>>> 9eb4935ec50a9c57d9a4e84e237f2ded02689f2e
 }
