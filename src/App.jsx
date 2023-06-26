57dd0086b27a430a8fe0e5cd8ae42bb9c99624d8
Alirahmon777 <alirahmonuraliev007@gmail.com>
2023-06-26 14:53:29 +0500

    svg-keldi

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,23 @@
-<<<<<<< HEAD
 import React from 'react';
 import Login from './pages/Login/Login';
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/Home';
-
-=======
-import React from "react";
-import Login from "./pages/Login/Login";
-import { Routes, Route } from "react-router-dom";
-import LANG from "./components/language/language";
->>>>>>> 843562fc82b72f45575a68a6294ff1930cc1eb2d
+import LANG from './components/language/language';
 export default function App() {
   return (
-<<<<<<< HEAD
-    <div className='wrapper'>
-      {/* <Aside /> */}
-      <main className='main'>
-        <Header setLanguage={setLanguage} setMode={setMode} />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/students' element={<Students />} />
-          <Route path='/payment' element={<Payment />} />
-          <Route path='*' element={<Error />} />
-        </Routes>
-      </main>
-    </div>
-=======
     <context.Provider value={{ language, mode, LANG }}>
-      <div className="wrapper">
+      <div className='wrapper'>
         <Aside />
-        <main className="main">
+        <main className='main'>
           <Header setLanguage={setLanguage} setMode={setMode} />
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={Login} />
-            <Route path="/students" element={<Students />} />
-            <Route path="/payment" element={<Payment />} />
-            <Route path="*" element={<Error />} />
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={Login} />
+            <Route path='/students' element={<Students />} />
+            <Route path='/payment' element={<Payment />} />
+            <Route path='*' element={<Error />} />
           </Routes>
         </main>
       </div>
     </context.Provider>
->>>>>>> 843562fc82b72f45575a68a6294ff1930cc1eb2d
   );
 }
