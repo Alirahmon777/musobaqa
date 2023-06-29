2f5b677d415b0b09593d520a311c63b1551e17cb
TemurEShtemirov <temureshtemirov10@gmail.com>
2023-06-29 10:00:18 +0500

    Asalomu alaykum

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,38 @@
-import React, { useContext } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { Context } from './context/Context';
-import Header from './components/Header';
-import Home from './pages/Home/Home';
-import Payment from './pages/Payment/Payment';
-import Error from './pages/Error/Error';
-import Aside from './components/Sidebar';
-import { AuthContext } from './context/AuthContext';
-import Login from './pages/Login/Login';
+import React, { useContext } from "react";
+import { Routes, Route } from "react-router-dom";
+import { Context } from "./context/Context";
+import Header from "./components/Header";
+import Home from "./pages/Home/Home";
+import Payment from "./pages/Payment/Payment";
+import Error from "./pages/Error/Error";
+import Aside from "./components/Sidebar";
+import { AuthContext } from "./context/AuthContext";
+import Login from "./pages/Login/Login";
 
 export default function App() {
   const { mode } = useContext(Context);
   const { isLogin } = useContext(AuthContext);
 
   return (
-    <div className='wrapper'>
-      <div className='flex'>
+    <div className="wrapper">
+      <div className="flex">
         <Aside />
         <main
-          className={'main w-full ' + (mode ? 'bg-white' : 'linear-bg__dark')}
+          className={"main w-full " + (mode ? "bg-white" : "linear-bg__dark")}
         >
           <Header />
           <Routes>
             {isLogin ? (
               <>
-                <Route path='/' element={<Home />} />
-                <Route path='/students' element={<Students />} />
-                <Route path='/payment' element={<Payment />} />
-                <Route path='*' element={<Error />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/students" element={<Students />} />
+                <Route path="/payment" element={<Payment />} />
+                <Route path="*" element={<Error />} />
               </>
             ) : (
               <>
                 {/* <Route path='*' element={<Login />} / > */}
-                <Route path='/login' element={<Login />} />
+                <Route path="/login" element={<Login />} />
               </>
             )}
           </Routes>
@@ -41,4 +41,3 @@ export default function App() {
     </div>
   );
 }
-
